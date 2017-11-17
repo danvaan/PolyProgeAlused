f4958d9584770486633fbf712cac801335cc15a4
danvaan <danvaan@users.noreply.github.com>
2017-11-17 13:45:17 +0200

    Tunnitööd

---
+++ Tunnitööd/17NovSolution1/17NovEsimeneTunnitöö/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _17NovEsimeneTunnitöö
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*GITHUB TEST 1
+             */
+
+
+        }
+    }
+}
